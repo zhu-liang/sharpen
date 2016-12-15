2fc84ee40bc58e2cb24a77759e340c2c0db0b8a3
Zhu Liang <zlux2357@gmail.com>
2016-12-15 23:35:24 +0800

    Initial Commit

---
+++ sharpenyourself/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  sharpenyourself
+//
+//  Created by zl on 15/12/16.
+//  Copyright © 2016年 zhu-liang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
